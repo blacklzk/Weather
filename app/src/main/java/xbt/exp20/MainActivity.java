8bf16b33f59ee2dbbe15f4b29445c1da5533d9fc
Your Name <you@example.com>
2018-04-25 14:20:29 +0800

    天气预报

---
+++ app/src/main/java/xbt/exp20/MainActivity.java
@@ -0,0 +1,18 @@
+package xbt.exp20;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xbt.exp20.gson.Weather;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
